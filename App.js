253efc9bc50a1682c4bcdbf4fc2612f7862f4401
Liam O <lokanedeveloper@gmail.com>
2023-01-19 08:05:21 +1100

    pushing updates

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Dimensions, Image} from 'react-native';
+import {Dimensions, Image, StyleSheet, View} from 'react-native';
 import {PanGestureHandler, State} from 'react-native-gesture-handler';
 import Animated, {
   add,
@@ -24,11 +24,11 @@ import {
 const {width, height} = Dimensions.get('window');
 
 export const assets = [
-  require('./assets/1.jpg'),
-  require('./assets/2.jpg'),
   require('./assets/3.jpg'),
+  require('./assets/2.jpg'),
   require('./assets/4.jpg'),
   require('./assets/5.jpg'),
+  require('./assets/1.jpg'),
 ];
 
 const snapPoints = assets.map((_, i) => i * -width);
@@ -36,7 +36,7 @@ const snapPoints = assets.map((_, i) => i * -width);
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'orange',
+    backgroundColor: 'black',
   },
   pictures: {
     width: width * assets.length,
@@ -61,9 +61,7 @@ const App = () => {
   const offsetX = useValue(0);
   const translateX = useValue(0);
   const {gestureHandler, state, velocity, translation} = usePanGestureHandler();
-
   const to = snapPoint(translateX, velocity.x, snapPoints);
-
   useCode(
     () => [
       cond(eq(state, State.ACTIVE), [
@@ -84,12 +82,12 @@ const App = () => {
     <View style={styles.container}>
       <PanGestureHandler {...gestureHandler}>
         <Animated.View style={StyleSheet.absoluteFill}>
-          <Animated.View style={(styles.pictures, {transform: [{translateX}]})}>
-            {assets.map(source => {
+          <Animated.View style={[styles.pictures, {transform: [{translateX}]}]}>
+            {assets.map(source => (
               <View key={source} style={styles.picture}>
                 <Image style={styles.image} {...{source}} />
-              </View>;
-            })}
+              </View>
+            ))}
           </Animated.View>
         </Animated.View>
       </PanGestureHandler>
